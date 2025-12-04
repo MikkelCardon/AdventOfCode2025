210068ee02827ee3f15e9195e060a17737f8a24f
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-12-04 19:25:04 +0100

    Day 1 - part 1

---
+++ day1/part1.js
@@ -0,0 +1,26 @@
+import { log } from 'console'
+import fs from 'fs'
+
+const text = fs.readFileSync('./day1/input1.txt', 'utf8')
+const lines = text.split("\n")
+
+
+let zero_counter = 0
+
+let pointer = 50
+
+lines.forEach(element => {
+    let direction = element.charAt(0)
+    let number = parseInt(element.substring(1))
+
+    if(direction === 'L') number *= -1
+    
+    pointer = (pointer + number) % 100
+    
+    if(pointer === 0) zero_counter++
+    
+});
+
+console.log(zero_counter);
+
+
